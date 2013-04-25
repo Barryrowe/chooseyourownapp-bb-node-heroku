d3472080470f3d53773f8fc16e51e5de92aa1b93
Barryrowe <barryrowe@gmail.com>
2013-04-25 10:37:30 -0400

    Adding question adding and templated questions

--- scripts/views/new_question.js
+++ scripts/views/new_question.js
@@ -1,13 +1,24 @@
 App.Views.NewQuestion = Backbone.View.extend({
+	events:{
+		"click #addItem":"addItem"
+	},
 	initialize: function(options){
 		App.currentUser.on("change", this.render, this);
 		this.render();
 	},
 	render: function(){
 		var uname = App.currentUser.get("userName");
-		console.log(uname);
 		this.$("#asker").text(uname);
 		this.$el.toggleClass("hidden", !App.currentUser.isLoggedIn());
 		return this;
+	},
+	addItem: function(){
+		var question = {
+			text: this.$("#newQuestion").val(),
+			userName: App.currentUser.get("userName")
+		};
+
+		this.model.add(new App.Models.Question(question));
+		this.$("#newQuestion").val('');
 	}
 });
